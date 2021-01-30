d70c140077b1cc7e7fc61cba97fe3ec03f2f260f
Akshat Lunia <luniaakshat@gmail.com>
2021-01-30 15:43:53 -0800

    Update leap_years.py

--- leap_years.py
+++ leap_years.py
@@ -1,6 +1,6 @@
 print("Type only the integer year that you wish to check for leap year")
 year = input()
-    yearInt = int(year)
+yearInt = int(year)
 if(yearInt%400 == 0):
     print("The year",yearInt,"is a 400-year leap year")
 else:
