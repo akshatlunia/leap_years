62b5f93d1b01806d8ba8220a3a8cc24a19f1858f
Akshat Lunia <luniaakshat@gmail.com>
2021-01-30 15:43:17 -0800

    Create leap_years.py

---
+++ leap_years.py
@@ -0,0 +1,17 @@
+print("Type only the integer year that you wish to check for leap year")
+year = input()
+    yearInt = int(year)
+if(yearInt%400 == 0):
+    print("The year",yearInt,"is a 400-year leap year")
+else:
+    if(yearInt%100 == 0):
+        print("The year",yearInt,"is not a leap year")
+        import sys
+        sys.exit()
+    else:
+        if(yearInt%4 == 0):
+            print("The year",yearInt,"is a leap year")
+            import sys
+            sys.exit()
+        else:
+            print("The year",yearInt,"is not a leap year")
